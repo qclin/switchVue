753f019c801686c87753e1b35a396a4bf020b0db
Qiao Lin <qiaoclin@gmail.com>
2015-07-10 11:23:28 -0400

    pagination

--- app.js
+++ app.js
@@ -13,16 +13,31 @@ var challenge = angular.module('movieApp', ['ui.bootstrap'])
 
       }
   })
-  .controller('MainCtrl', function($scope, $window, $http, $modal, $log, $timeout){
-    
-    $http.get("http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c")
+  .controller('MainCtrl', function($scope, $window, $http, $modal, $log, $timeout, $filter){
+    var i = 1; 
+    getMovies();
+
+    $scope.prev = function(){
+      if (i==1){
+        alert("these are the latest now_playing movies ! ");
+      }else{
+        i-= 1 
+        getMovies();
+      }
+    }
+    $scope.next = function(){
+      i+= 1 
+      getMovies();
+    }
+    function getMovies(){
+    $http.get("http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&page="+i)
       .success(function(data){
-        console.log(data);
-        $scope.movies = data.results; 
+        $scope.movies = data.results
       })
       .error(function(data){
         console.log(data);
       });
+    }
 
     // MODAL WINDOW
     $scope.open = function (_movie) {

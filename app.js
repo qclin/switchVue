9ce1775f4dfb6524f0d7c6d0463f8c3d846abf20
Qiao Lin <qiaoclin@gmail.com>
2015-07-10 00:28:41 -0400

    modal

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-var challenge = angular.module('movieApp', [])
-  .controller('MainCtrl', function($scope, $window, $http){
+var challenge = angular.module('movieApp', ['ui.bootstrap'])
+  .controller('ModalCtrl', function($scope, $modalInstance, movie){
+
+      var backgroundImage = "url(http://image.tmdb.org/t/p/w1280" + movie.backdrop_path + ") no-repeat"  
+     document.body.style.background = backgroundImage;  
+
+      $scope.movie = movie; 
+      $scope.exit = function(){
+        $modalInstance.close();
+        document.body.style.background = "";
+      }
+  })
+  .controller('MainCtrl', function($scope, $window, $http, $modal, $log, $timeout){
     
     $http.get("http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c")
       .success(function(data){
@@ -9,5 +20,23 @@ var challenge = angular.module('movieApp', [])
       .error(function(data){
         console.log(data);
       });
-  
+
+    // MODAL WINDOW
+    $scope.open = function (_movie) {
+      var modalInstance = $modal.open({
+
+        controller: "ModalCtrl",
+        templateUrl: 'selectedMovie.html',
+        resolve: {
+          movie: function(){
+              return _movie;
+          },
+        }
+      });
+
+    };
+
+
+
+
   });
